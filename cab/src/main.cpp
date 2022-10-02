0908733189075c2de1f0baf10a7129b88191391c
vonkaa <bartVonk23@gmail.com>
2022-10-02 23:46:41 +0200

    Added blinky

--- cab/src/main.cpp
+++ cab/src/main.cpp
@@ -1,6 +1,16 @@
+#define F_CPU 3333333
 
+#include <stdio.h>
+#include <avr/io.h>
+#include <avr/delay.h>
 
-int main() 
-{
+int main(void) {
+    PORTA.DIR |= PIN7_bm;
 
+    for(int i = 0; i < 1000; i++) {
+        PORTA.OUT |= PIN7_bm;
+        _delay_ms(500);
+        PORTA.OUT &= ~PIN7_bm;
+        _delay_ms(500);
+    }
 }
