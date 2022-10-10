b9e56be4bac687cdb7df08e5c2744d14c23da662
vonkaa <bartVonk23@gmail.com>
2022-10-10 17:35:54 +0200

    Added setup function

--- cab/src/main.cpp
+++ cab/src/main.cpp
@@ -7,22 +7,32 @@
 #include <SCCP.h>
 #include <string.h>
 
+void setup();
+
 SCCP sccp;
 
 int main(void) {
+    setup();
+    sccp.init();
+
+    while(1);
+}
+
+void setup() 
+{
+    // Set PA7 as output
     PORTA.DIR = PIN7_bm;
     PORTA.OUT |= PIN7_bm;
 
+    // Set all gates as inputs and enable pullups with inverted logic
     PORTC.DIRCLR = GATES; 
     PORTC.PIN0CTRL |= PORT_INVEN_bm | PORT_PULLUPEN_bm;
     PORTC.PIN1CTRL |= PORT_INVEN_bm | PORT_PULLUPEN_bm;
     PORTC.PIN2CTRL |= PORT_INVEN_bm | PORT_PULLUPEN_bm;
     PORTC.PIN3CTRL |= PORT_INVEN_bm | PORT_PULLUPEN_bm;
 
+    // Enable global interrupts
     SREG |= CPU_I_bm;
-    sccp.init();
-
-    while(1);
 }
 
 ISR(USART0_RXC_vect) 
