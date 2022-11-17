5692de97e547e36becee69a1f769d11721851688
Stefan de Kraker <stefandekraker1999@gmail.com>
2022-11-18 00:05:16 +0100

    Simple setup of the Grid compose view and ViewModel, implements the new Threads in the repository to send and receive data from a bluetooth device

--- app/src/main/java/com/floppa/stackcabinet/ui/GridCompose.kt
+++ app/src/main/java/com/floppa/stackcabinet/ui/GridCompose.kt
@@ -1,7 +1,10 @@
 package com.floppa.stackcabinet.ui
 
+import android.app.Activity
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.NavHostController
@@ -14,9 +17,43 @@ fun GridCompose(
     viewModel: GridViewModel,
     lifeCycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
 ) {
+    var device by remember { mutableStateOf("") }
+
+    val activity = LocalContext.current as Activity
+
+    val context = LocalContext.current
+
+    val lifecycleOwner = LocalLifecycleOwner.current
 
     CenterElement {
         Text(text = "Grid Compose")
+
+        Button(onClick = {
+            // HERE make Connection
+            viewModel.startConnection()
+        }) {
+            Text(text = "Connect to ESP")
+        }
+        Button(onClick = {
+            // HERE make break active connection
+            viewModel.stopConnection()
+        }) {
+            Text(text = "Disconnect to ESP")
+        }
+        Button(onClick = {
+            // HERE send some sample data
+            viewModel.writeData()
+        }) {
+            Text(text = "Send data to ESP")
+        }
     }
+
+
+
+
+
+
+
+
 
 }
--- app/src/main/java/com/floppa/stackcabinet/ui/viewmodel/GridViewModel.kt
+++ app/src/main/java/com/floppa/stackcabinet/ui/viewmodel/GridViewModel.kt
@@ -1,9 +1,25 @@
 package com.floppa.stackcabinet.ui.viewmodel
 
+import android.bluetooth.BluetoothDevice
 import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.floppa.stackcabinet.repository.BluetoothRepository
 
 class GridViewModel(context: Context): ViewModel() {
+    private val repository = BluetoothRepository(context = context)
 
+    fun getPairedBase(): BluetoothDevice? {
+        return repository.getPairedBase()
+    }
+    fun startConnection(){
+        repository.startClient(repository.getPairedBase())
+    }
 
+    fun stopConnection(){
+        repository.stopClient(repository.getPairedBase())
+    }
+
+    fun writeData(){
+        repository.writeToStream("Hello Back".toByteArray(Charsets.UTF_8))
+    }
 }
