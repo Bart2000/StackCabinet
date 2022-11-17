066a52a10067a09a5c0ec075d169f3d854b37e24
Stefan de Kraker <stefandekraker1999@gmail.com>
2022-11-18 00:02:56 +0100

    Added connect and connected Threads, these Threads are used to, 1. setup a connection with a Bluetooth devices, and 2. to open a Input and Output stream to send and receive data

--- app/src/main/java/com/floppa/stackcabinet/repository/BluetoothRepository.kt
+++ app/src/main/java/com/floppa/stackcabinet/repository/BluetoothRepository.kt
@@ -6,27 +6,35 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothSocket
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.os.Build
 import android.util.Log
 import com.floppa.stackcabinet.hasPermissions
 import com.floppa.stackcabinet.models.Constants.name_stackcabinet_base
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import java.io.IOException
+import java.io.InputStream
 import java.io.OutputStream
+import java.nio.charset.StandardCharsets
 import java.util.*
 
 
 @SuppressLint("MissingPermission")
 class BluetoothRepository(private val context: Context) {
+    /**
+     * Get the [BluetoothManager] and [BluetoothAdapter]
+     */
     private val bluetoothManager: BluetoothManager = context.getSystemService(BluetoothManager::class.java)
     val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
 
-    private val defaultDispatcher: CoroutineDispatcher = Dispatchers.IO
+    /**
+     * Get a [Set] of paired devices ([BluetoothDevice]) to the phone
+     */
+    private val devices = bluetoothAdapter?.bondedDevices as Set<BluetoothDevice>
 
-    private val BLUETOOTH_SPP: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+    private var socket: BluetoothSocket? = null
+
+    private val bluetoothSppUuid: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
 
     /**
      * Check if the devices had bluetooth
@@ -82,38 +90,176 @@ class BluetoothRepository(private val context: Context) {
         }
     }
 
-
-    suspend fun setupSocket(device: BluetoothDevice) {
-        // Get a BluetoothSocket to connect with the given BluetoothDevice
-        withContext(defaultDispatcher) {
-            var socket: BluetoothSocket? = null
-            var out: OutputStream? = null
-            try {
-                if (context.hasPermissions(Manifest.permission.BLUETOOTH_CONNECT)) {
-                    socket = device.createRfcommSocketToServiceRecord(BLUETOOTH_SPP)
-                    socket.connect();
-                    out = socket.outputStream;
-                } else {
-
-                }
-            } catch (e: IOException) {
-                Log.e("BluetoothRepository", "$e")
-            }
-        }
-    }
-
     /**
      * @return true/false based on if a StackCabinet base is already paired to the phone
      */
     fun isPaired(): Boolean {
-        val results = bluetoothAdapter?.bondedDevices as Set<BluetoothDevice>
-        results.forEach {
-            println(it.name)
-            if (it.name.contains(name_stackcabinet_base)) {
-                return true
+        devices.forEach {
+            if (context.hasPermissions(Manifest.permission.BLUETOOTH_CONNECT)) {
+                if (it.name.contains(name_stackcabinet_base)) {
+                    return true
+                }
             }
         }
         return false
     }
 
+    /**
+     * Look for a paired devices that contains the correct name
+     * @return if found [BluetoothDevice], else null
+     */
+    fun getPairedBase(): BluetoothDevice? {
+        devices.forEach {
+            if (context.hasPermissions(Manifest.permission.BLUETOOTH_CONNECT)) {
+                if (it.name.contains(name_stackcabinet_base)) {
+                    return it
+                }
+            }
+        }
+        return null
+    }
+
+    /**
+     * open a connection with the [ConnectThread] and run the Thread
+     */
+    fun startClient(device: BluetoothDevice?) {
+        Log.d(TAG, "startClient: Started.")
+        device?.let { ConnectThread(it) }?.start()
+    }
+
+    /**
+     * Close the connection that is made with the [ConnectThread]
+     */
+    fun stopClient(device: BluetoothDevice?) {
+        if (device != null) {
+            ConnectThread(device).cancel()
+        }
+    }
+
+    /**
+     * Start the [InputStream] and [OutputStream] to the [BluetoothDevice]
+     */
+    private fun startStream(socket: BluetoothSocket){
+        this.socket = socket
+        ConnectedThread(socket).start()
+    }
+
+    /**
+     * Write a [ByteArray] to the [OutputStream] of [ConnectedThread]
+     */
+    fun writeToStream(bytes: ByteArray){
+        socket?.let { ConnectedThread(it) }?.write(bytes)
+    }
+
+    /**
+     * Close the stream of [ConnectedThread]
+     */
+    fun closeStream() {
+        socket?.let { ConnectedThread(it) }?.cancel()
+    }
+
+    /**
+     * Inner class to make a connection with the StackCabinet Base.
+     * When connection is established, [ConnectedThread] is started.
+     * Using the [BluetoothDevice], a [BluetoothSocket] is created, work associated with
+     * the connection is done in a separate thread.
+     */
+    private inner class ConnectThread(device: BluetoothDevice) : Thread() {
+
+        private val mmSocket: BluetoothSocket? by lazy(LazyThreadSafetyMode.NONE) {
+            device.createRfcommSocketToServiceRecord(bluetoothSppUuid)
+        }
+
+        override fun run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            bluetoothAdapter?.cancelDiscovery()
+
+            mmSocket?.let { socket ->
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                Log.i("ConnectThread", "connecting to Socket")
+                socket.connect()
+
+                startStream(socket)
+            }
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        fun cancel() {
+            try {
+                mmSocket?.close()
+            } catch (e: IOException) {
+                Log.e(TAG, "Could not close the client socket", e)
+            }
+        }
+
+    }
+
+    /**
+     * Inner class to make a [InputStream] and [OutputStream] between the StackCabinet base and
+     * the phone itself.
+     * When the Thread is started a while loops keeps checking for incoming data
+     * With [write] Data is written to the [BluetoothSocket]
+     */
+    private inner class ConnectedThread(private val mmSocket: BluetoothSocket) : Thread() {
+
+        private val mmInStream: InputStream = mmSocket.inputStream
+        private val mmOutStream: OutputStream = mmSocket.outputStream
+        private val mmBuffer: ByteArray = ByteArray(256) // mmBuffer store for the stream
+
+        override fun run() {
+
+            // Keep listening to the InputStream until an exception occurs.
+            while (true) {
+                // Read from the InputStream.
+                try {
+                    mmInStream.read(mmBuffer)
+                } catch (e: IOException) {
+                    Log.d(TAG, "Input stream was disconnected", e)
+                    break
+                }
+                val s = String(mmBuffer, StandardCharsets.UTF_8)
+                println(mmBuffer)
+                println(s)
+
+                // Send the obtained bytes to the UI activity.
+//                val readMsg = handler.obtainMessage(
+//                    Companion.MESSAGE_READ, numBytes, -1,
+//                    mmBuffer)
+//                readMsg.sendToTarget()
+            }
+        }
+
+        // Call this from the main activity to send data to the remote device.
+        fun write(bytes: ByteArray) {
+            try {
+                mmOutStream.write(bytes)
+            } catch (e: IOException) {
+                Log.e(TAG, "Error occurred when sending data", e)
+
+//                // Send a failure message back to the activity.
+//                val writeErrorMsg = handler.obtainMessage(Companion.MESSAGE_TOAST)
+//                val bundle = Bundle().apply {
+//                    putString("toast", "Couldn't send data to the other device")
+//                }
+//                writeErrorMsg.data = bundle
+//                handler.sendMessage(writeErrorMsg)
+//                return
+            }
+
+            // Share the sent message with the UI activity.
+//            val writtenMsg = handler.obtainMessage(
+//                Companion.MESSAGE_WRITE, -1, -1, mmBuffer)
+//            writtenMsg.sendToTarget()
+        }
+
+        // Call this method from the main activity to shut down the connection.
+        fun cancel() {
+            try {
+                mmSocket.close()
+            } catch (e: IOException) {
+                Log.e(TAG, "Could not close the connect socket", e)
+            }
+        }
+    }
 }
