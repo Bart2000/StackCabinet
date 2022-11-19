0a9cbb60a1a0e8818b23d7db1b61f085672b9f45
vonkaa <bart.vonk2@hva.nl>
2022-11-19 15:00:37 +0100

    Started with PWM

--- cab/src/main.cpp
+++ cab/src/main.cpp
@@ -13,13 +13,36 @@ SCCP sccp;
 
 int main(void) {
     setup();
-    sccp.init();
+    //sccp.init();
 
     while(1);
 }
 
 void setup() 
 {
+    PORTB.DIR |= PIN0_bm;
+    PORTB.OUT |= PIN0_bm;
+    
+    // TCA0_SPLIT_CTRLESET |= TCA_SPLIT_CMD;
+    // TCA0_SPLIT_CTRLESET |= TCA_SPLIT_CMD_RESET_gc;
+    // TCA0_SPLIT_CTRLESET |= TCA_SPLIT_CMD_RESTART_gc;
+   
+   TCA0.SINGLE.CTRLA &= ~(TCA_SINGLE_ENABLE_bm);  
+   
+    TCA0.SINGLE.CTRLESET = TCA_SINGLE_CMD_RESET_gc; 
+
+    TCA0.SPLIT.CTRLD = TCA_SPLIT_SPLITM_bm;
+    TCA0.SPLIT.LPER = 0xCF;
+    TCA0.SPLIT.HPER = 0xCF;
+    TCA0.SPLIT.LCMP0 = 0x40;
+    TCA0.SPLIT.HCMP0 = 0x40;
+    TCA0.SPLIT.CTRLB |= TCA_SPLIT_LCMP0EN_bm | TCA_SPLIT_HCMP0EN_bm;
+    TCA0.SPLIT.CTRLA |= TCA_SPLIT_CLKSEL_DIV16_gc | TCA_SPLIT_ENABLE_bm;
+
+    
+
+    return;
+
     // Set PA7 as output
     PORTA.DIR = PIN7_bm;
     PORTA.OUT |= PIN7_bm;
